adcc61765aaa96384ed961b9ea449658b1f64a05
Foram <foram@thinkbiz.co.in>
2021-03-21 16:07:41 +0530

    first commit

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,13 @@
+class RobotsController < ApplicationController
+
+	def new
+		@robot = Robot.new
+	end
+	
+	def create
+		@robot = Robot.new(params[:robot])
+    @robot.execute_commands!
+    render :new
+	end	
+
+end
